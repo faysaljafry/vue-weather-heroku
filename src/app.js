b34710dcb96b6671cb03c8b51fc18bc3b8b2069b
faysaljafry <faysaljafryofficial@gmail.com>
2021-11-08 16:29:36 +0500

    added dist configuration

--- src/app.js
+++ src/app.js
@@ -100,7 +100,11 @@ app.get('./updateWeatherdata', (req, res) => {
   });
 });
 
-app.get('/', function (req, res) {
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
